de21fbc457082183bb3a9f58f7becc1b573dadb7
Amiruddin <muhammad.amiruddin.112@gmail.com>
2023-10-17 13:46:57 +0800

    fix filament panel permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\UserType;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
@@ -39,6 +43,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if($this->hasRole(UserType::ADMIN)){
+            return true;
+        }
+        return false;
+    }
 
     public function school(){
         return $this->belongsTo(School::class);
